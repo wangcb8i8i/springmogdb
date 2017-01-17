cd1467ecd5020136c381fafb9041396d262b2953
wangcb8i8i <wangcb8i8i@gmail.com>
2017-01-17 15:00:52 +0800

    add files

---
+++ src/main/java/wangcb8i8i/springmgodb/dao/support/UserDao.java
@@ -0,0 +1,8 @@
+package wangcb8i8i.springmgodb.dao.support;
+
+import wangcb8i8i.springmgodb.dao.BaseDao;
+import wangcb8i8i.springmgodb.model.User;
+
+public interface UserDao extends BaseDao<User>{  
+	  
+}
---
+++ src/main/java/wangcb8i8i/springmgodb/model/User.java
@@ -0,0 +1,17 @@
+package wangcb8i8i.springmgodb.model;
+
+import java.io.Serializable;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Document
+@Data
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String id;
+	private String name;
+	private int age;
+	private String password;
+}
